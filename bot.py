06a261c2e0403d44512f80f9a261110ae2c4a51a
Rohit Namboothiri <rohitchandran88@gmail.com>
2018-01-04 18:04:52 +0530

    image download feature added

--- bot.py
+++ bot.py
@@ -4,16 +4,24 @@ import telepot
 from telepot.loop import MessageLoop
 from time import gmtime, strftime
 import datetime
-
-
+from conscience import analyze
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
 
     if content_type == 'text' and msg['text'].lower()=='time' :
 	        	bot.sendMessage(chat_id,currenttime())
+    elif content_type=='photo':
+                name = time.strftime("%Y%m%d-%H%M%S")
+                filename = f"{name}.jpg"
+                bot.download_file(msg['photo'][-1]['file_id'], filename)
     else:
-    	bot.sendMessage(chat_id,"Sorry,I didn't get you.I am still in learning phase..!!!")
+    	bot.sendMessage(chat_id,analyze(msg['text']))
+
+
+
+
+
        
 def currenttime():
 	now = datetime.datetime.now() 
